b2f85b9573c63e18633fa94ca3ec9aae127c7767
mjferstl <mathiasferstl@gmail.com>
2020-04-30 22:52:07 +0200

    Entwurf fuer die Speicherung von aktienbezogenen Informationen in separaten Dateien

--- scripts/createStockAnalysisPDF.py
+++ scripts/createStockAnalysisPDF.py
@@ -6,18 +6,68 @@ from classes.Stock import Stock, StockIndex
 from classes.StockCompare import StockCompare, StockComparePDF
 from classes.StockAnalyzer import StockAnalyzer
 
-symbol = 'MSFT'
+import json
 
-stock = Stock(symbol=symbol)
-StockAnalyzer(stock,StockIndex.DOW_JONES_INDEX_SYMBOL).printAnalysis()
+### Eingabe Start
 
-msftCompare = StockCompare(symbol)
-peerGroupList = msftCompare.getPeerGroup(symbol=symbol)
-df = msftCompare.getPeerGroupChangePrc(peerGroupList)
-mainValueCompareDF = msftCompare.comparePeerGoupMainValues(peerGroupList)
+stockName = 'Microsoft'
 
-pdf_filename = symbol + '_peer_group_compare.pdf'
-scPDF = StockComparePDF(pdf_filename)
-scPDF.addPlot(df,xlabel='Date',ylabel='stock value growth in %',title=symbol + ' - Peer group comparison')
-scPDF.addTable(mainValueCompareDF)
-scPDF.closePDF()
+
+### Eingabe Ende
+
+
+stocksFile = "scripts/stocks.json"
+with open(stocksFile) as f:
+    stockJSON = json.load(f)
+
+stock = ''
+for s in stockJSON["Stocks"]:
+    if s["Name"] == stockName:
+        stock = s
+        break
+
+if s == '':
+    print('please add the stock in the file ' + stocksFile)
+    sys.exit(1000)
+
+symbol = stock["Symbol"]
+stockIndex = stock["Index"]
+
+data_file = 'scripts/' + stock["data_file"]
+if os.path.isfile(data_file):
+    with open(data_file) as f:
+        stockEstimates = json.load(f)
+    annualGrowthPrc = stockEstimates["annualGrowthPrc"]
+else:
+    print('\n +++ no data file +++\n')
+    sys.exit(1000)
+
+index = ''
+# get the index
+for i in stockJSON["Index"]:
+    if i["Name"] == stockIndex:
+        index = i
+        break
+
+if s == '':
+    print('please add the index "' + stockIndex + ' in the file ' + stocksFile)
+    sys.exit(1000)
+
+
+
+# Laden der Daten
+stock = Stock(symbol=symbol,growthRateAnnualyPrc=int(annualGrowthPrc))
+
+# Analysieren der Daten
+StockAnalyzer(stock,index["Symbol"]).printAnalysis()
+
+#msftCompare = StockCompare(symbol)
+#peerGroupList = msftCompare.getPeerGroup(symbol=symbol)
+#df = msftCompare.getPeerGroupChangePrc(peerGroupList)
+#mainValueCompareDF = msftCompare.comparePeerGoupMainValues(peerGroupList)
+
+#pdf_filename = symbol + '_peer_group_compare.pdf'
+#scPDF = StockComparePDF(pdf_filename)
+#scPDF.addPlot(df,xlabel='Date',ylabel='stock value growth in %',title=symbol + ' - Peer group comparison')
+#scPDF.addTable(mainValueCompareDF)
+#scPDF.closePDF()
